c6330f32edce4db100fb4dba01c0e3345715f538
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2023-02-19 21:28:12 +0000

    tarefa 2 do tpc1 + manifest

--- TPC1/cidades/generator.pl
+++ TPC1/cidades/generator.pl
@@ -11,8 +11,7 @@ use constant JSON_PATH     => 'mapa.json';
 use constant OUTPUT_PATH   => 'output.html';
 
 use constant BEGIN_HTML    =>
-q{
-<html>
+q{<html>
     <head>
         <title>Mapa Virtual</title>
         <meta charset="utf-8"/>
@@ -23,26 +22,25 @@ q{
             <tr>
                 <td witdh="30%" valign="top">
                     <h3><a name="indice"/>Índice</h3>
-                    <!-- Lista com o Índice -->
-};
+                    <!-- Lista com o Índice -->};
 
 use constant MIDDLE_HTML   =>
 q{
                 </td>
                 <td width="70%">
-                    <!---- Informação das Cidades -->
-};
+                    <!---- Informação das Cidades -->};
 
 use constant END_HTML      =>
-q{
-                </td>
+q{              </td>
             </tr>
         </table>
     </body>
-</html>
-};
+</html>};
 
-use constant REF_TEMPLATE        => '                <li><a href="#__id__">__city_name__</a></li>';
+
+use constant REF_TEMPLATE        => 
+    '                    <li><a href="#__id__">__city_name__</a></li>';
+
 use constant LIST_TEMPLATE_BEGIN =>
 q{
                     <a name="__id__"/>
@@ -51,21 +49,17 @@ q{
                     <p><b>descrição:</b> __description__</p>
                     <p><b>distrito:</b> __district__</p>
 
-                    <h4>Ligações</h4>
-};
+                    <h4>Ligações</h4>};
 
 use constant LIST_TEMPLATE_LINKS =>
-q{
-                    <li><a href="#__neighbour_id__">__neighbour_city_name__</a>}
-.
+q{                    <li><a href="#__neighbour_id__">__neighbour_city_name__</a>} .
 q{ (__distance__kms de distância)</li>};
 
 use constant LIST_TEMPLATE_END   =>
 q{
                     <center>
                         <hr witdh="80%"/>
-                    </center>
-};
+                    </center>};
 
 
 sub get_json_str(){
@@ -90,7 +84,7 @@ sub write_html($cities_ref, $graph_ref, $id_to_name_ref){
 
     open my $fh, '>:utf8', OUTPUT_PATH or die "$!";
 
-    print $fh BEGIN_HTML;
+    say $fh BEGIN_HTML;
 
     foreach(@cities){
 
@@ -101,7 +95,7 @@ sub write_html($cities_ref, $graph_ref, $id_to_name_ref){
         say $fh $template;
     }
 
-    print $fh MIDDLE_HTML;
+    say $fh MIDDLE_HTML;
 
     foreach(@cities){
 
@@ -115,8 +109,14 @@ sub write_html($cities_ref, $graph_ref, $id_to_name_ref){
 
         say $fh $template;
 
+
         my $city_id = ${$_}{'id'};
-        foreach my $dest (keys %{$graph{$city_id}}){
+        my @sorted_dests = 
+            sort 
+                { $id_to_name{$a} cmp $id_to_name{$b} } 
+                keys %{$graph{$city_id}};
+
+        foreach my $dest (@sorted_dests){
 
             my $link_template = LIST_TEMPLATE_LINKS;
 
@@ -124,14 +124,14 @@ sub write_html($cities_ref, $graph_ref, $id_to_name_ref){
             $link_template =~ s/__neighbour_city_name__/$id_to_name{$dest}/g;
             $link_template =~ s/__distance__/${$graph{$city_id}}{$dest}/g;
 
-			say $fh $link_template;
+            say $fh $link_template;
         }
 
         say $fh LIST_TEMPLATE_END;
     }
 
+    say $fh END_HTML;
 
-    print $fh END_HTML;
 
     close $fh;
 }
@@ -149,21 +149,21 @@ sub get_hash_graph($links_ref){
     my $curr_city_id = '';
 
 
-    foreach my $ind (0..$#links){
+    foreach(@links){
 
-        if(${$links[$ind]}{'origem'} ne $curr_city_id){
+        if(${$_}{'origem'} ne $curr_city_id){
 
             unless($curr_city_id eq ''){
                 $origin_to_neighbours{$curr_city_id} = $neighbour_to_distance_ref;
             }
 
-            $curr_city_id = ${$links[$ind]}{'origem'};
+            $curr_city_id = ${$_}{'origem'};
             $neighbour_to_distance_ref = {};
         }
 
 
-        my $neighbour_city_id = ${$links[$ind]}{'destino'};
-        my $distance = ${$links[$ind]}{'distância'};
+        my $neighbour_city_id = ${$_}{'destino'};
+        my $distance = ${$_}{'distância'};
 
         ${$neighbour_to_distance_ref}{$neighbour_city_id} = $distance;
     }
@@ -179,9 +179,9 @@ sub get_id_to_name_hash($cities_ref){
 
     my $id_to_name_ref = {};
 
-    foreach my $c (@cities){
+    foreach(@cities){
 
-        ${$id_to_name_ref}{${$c}{'id'}} = ${$c}{'nome'};
+        ${$id_to_name_ref}{${$_}{'id'}} = ${$_}{'nome'};
     }
 
     return $id_to_name_ref;
